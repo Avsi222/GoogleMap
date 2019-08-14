e20c523ca3811d3998f1e8ef167633a1b64ae47a
Arseniy <dorogin97@yandex.ru>
2019-08-14 19:55:04 +0300

    Add auth and create draw route with save to realm

--- MapProject/App/AppDelegate.swift
+++ MapProject/App/AppDelegate.swift
@@ -19,6 +19,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
         // Override point for customization after application launch.
         GMSServices.provideAPIKey("AIzaSyBp-QTKz-xN9CmhAMU8_1tf5QV1HiDAvB0")
+        
         return true
     }
 
